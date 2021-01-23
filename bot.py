5f76c409a1c7635d87acc7acaf1128530dd0f16d
Tyzbit <3319104+tyzbit@users.noreply.github.com>
2021-01-22 21:04:46 -0500

    formatting

--- bot.py
+++ bot.py
@@ -13,14 +13,11 @@ class GameState:
     self.load_state_and_config()
 
   def load_state_and_config(self):
-    """
+    '''
     Initializes the game state by reading it from a file
-    """
+    '''
     self.current_dir = str(pathlib.Path(__file__).resolve().parent)
-    self.state={
-      "active_games": {},
-      "historical_game_summaries": {}
-    }
+    self.state={}
     config_file = f'{current_dir}/config.json'
     try:
       with open(config_file, 'r') as read_file:
@@ -44,7 +41,6 @@ class GameState:
     except Exception as e:
       logger.warning(f'State file not found at {state_file}, creating new state')
       self.save_game_state()
-
     return 'Successfully loaded game state and config'
 
   def get_game_state(self):
@@ -66,9 +62,8 @@ class GameState:
       try:
         f = open(self.current_dir + '/state.json', 'w')
       except Exception as e:
-        logger.error('Still unable to open state file after creating it, exiting...')
+        logger.error(f'Still unable to open state file after creating it, exception: {e}')
         sys.exit(1)
-
     try:
       f.write(json.dumps(self.state))
       f.close()
@@ -78,29 +73,66 @@ class GameState:
     return 'Successfully saved game state!'
 
   def get_summary(self):
-    active_games = len(self.state['active_games'])
-    historical_game_summaries = len(self.state['historical_game_summaries'])
-    players = 0
-    for active_game in self.state['active_games']:
-      if 'players' in active_game:
-        for players in active_game['players']:
-          players += 1
-    return f'{active_games} games being played by {players} players.  {total_games} total games played.'
+    if 'servers' in self.state:
+      active_games = 0
+      players = 0
+      historical_games = 0
+      for server in self.state['servers']:
+        active_games += len(self.state[server]['active_games'])
+        historical_games += len(self.state[server]['historical_games'])
+        for active_game in self.state[server]['active_games']:
+          if 'players' in active_game:
+            for players in active_game['players']:
+              players += 1
+      total_games = active_games + historical_games
+      return f'{active_games} games being played by {players} players.  {total_games} games played all time.'
+    else:
+      return 'There are no servers in the state file.  Is this a new bot?'
   
-  def add_new_game(self, game_code, embed):
-    self.active_games += { game_code: embed }
+  def add_new_game_session(self, game_code, message, embed):
+    self.state[message.guild]['active_games'][game_code] = {
+      'players': {
+        message.author: {
+          'embed': embed,
+          'player_color': embed.color
+        }
+      }
+    }
+    self.save_game_state()
+    return f'Game session {game_code} added!'
   
-  def get_existing_active_game(self, game_code, embed):
-    if game_code not in self.active_games:
+  def join_active_game(self, game_code, message, embed):
+    if not self.state[message.guild]['active_games'][game_code]:
+      logger.debug(f'{message.author} tried to join {game_code} but it doesn\'t exist')
+      message.channel.send('That game code does not exist')
+      return False
+    else:
+      game_code_players = self.state[message.guild]['active_games'][game_code]['players']
+      self.state[message.guild]['active_games'][game_code]['players'] = {**game_code_players,
+        message.author: {
+          'embed': embed,
+          'player_color': embed.color
+        }
+      }
+      self.save_game_state()
+      return f'You joined game session {game_code}!'
+  
+  def get_existing_active_game(self, game_code, server, embed):
+    if game_code not in self.state[server]['active_games']:
       logger.debug(f'Game code {game_code} not found in active games!')
     else:
-      return self.active_games[game_code]
+      return self.state[server]['active_games'][game_code]
   
   def get_all_game_codes(self):
-    active_games = self.active_games.keys()
-    historical_games = self.historical_games.keys()
-    all_games = active_games + historical_games
-    return all_games
+    if servers not in self.state:
+      return 'There are no servers in the state file.  Is this a new bot?'
+    else:
+      for server in self.state['servers']:
+        active_games = server['active_games']
+        historical_games = server['historical_games']
+        all_games = active_games + historical_games
+        return all_games
+
 
 async def start_new_game(game_state, message):
   valid_characters = string.ascii_uppercase
@@ -109,27 +141,35 @@ async def start_new_game(game_state, message):
     game_code_length = config['gameCodeLength']
   game_code_found = False
   tries = 0
-  while game_code_found = False:
-    proposed_game_code = ''.join(random.choice(letters) for i in range(game_code_length))
+  while game_code_found == False:
+    proposed_game_code = ''.join(random.choice(valid_characters) for i in range(game_code_length))
     tries += 1
     if proposed_game_code not in game_state.get_all_game_codes():
       game_code = proposed_game_code
       game_code_found = True
-    if tries = 10:
+    if tries == 10:
       logger.debug('Tried to generate a new code 10 times and failed')
       return False
   logger.debug(f'Starting new AU game ID {game_code}')
-  embedVar = discord.Embed(title="", description="Desc", color=0x00ff00)
-  #embedVar.add_field(name="Field1", value="hi", inline=False)
+  embedVar = discord.Embed(
+    title=f'Among Us Game {game_code}',
+    description='You are playing among us',
+    color=0x00ff00
+  )
+  #embedVar.add_field(name='Field1', value='hi', inline=False)
   returned_embed = await send_embed(message.author, embedVar)
-  game_state.add_new_game(game_code, returned_embed)
+  game_state.add_new_game_session(game_code, message, returned_embed)
 
-async def update_current_game(game_state, embed):
-  logger.debug('Updating current game')
-  embedVar = discord.Embed(title="Among us New", description="Desc", color=0x00ff00)
-  embedVar.add_field(name="Field1", value="hi", inline=False)
-  current_embed = game_state.get_existing_active_game(embed)
-  await current_embed.edit(embed=embedVar)
+async def join_active_game(game_state, message):
+  game_code = message.content.replace('!aujoin ', '')
+  logger.debug(f'{message.author} trying to join {game_code}')
+  embedVar = discord.Embed(
+    title=f'Among Us Game {game_code}',
+    description='You are playing among us',
+    color=0x00ff00
+  )
+  returned_embed = await send_embed(message.author, embedVar)
+  game_state.join_active_game(game_code, message, returned_embed)
 
 async def get_summary(game_state, message):
   logger.debug('Sending summary')
@@ -159,6 +199,13 @@ async def send_embed(user, embed):
   logger.debug('Sending a message')
   return await user.send(embed=embed)
 
+async def update_existing_embed(game_state, embed):
+  logger.debug('Trying to join an active game')
+  embedVar = discord.Embed(title='Among us New', description='Desc', color=0x00ff00)
+  embedVar.add_field(name='Field1', value='hi', inline=False)
+  current_embed = game_state.get_existing_active_game(embed)
+  await current_embed.edit(embed=embedVar)
+
 def main(game_state):
   logger.info('Starting bot...')
 
@@ -166,12 +213,12 @@ def main(game_state):
   client = discord.Client()
 
   possible_commands={
-    "!au": "start_new_game",
-    "!aujoin": "join_active_game",
-    "!auedit": "update_current_game",
-    "!ausummary": "get_summary",
-    "!ausave": "manually_save_state",
-    "!aureload": "load_state_and_config"
+    '!au': 'start_new_game',
+    '!aujoin': 'join_active_game',
+    '!auedit': 'update_current_game',
+    '!ausummary': 'get_summary',
+    '!ausave': 'manually_save_state',
+    '!aureload': 'load_state_and_config'
   }
   @client.event
   async def on_ready():
@@ -183,9 +230,10 @@ def main(game_state):
       return
 
     for command in possible_commands:
-      if message.content.startswith(command):
+      if message.content.split(' ')[0] == command:
         function = possible_commands[message.content.split(' ')[0]]
         call_function = globals()[function]
+        logger.debug(f'Calling {function}')
         await call_function(game_state, message)
 
   client.run(discordToken)
